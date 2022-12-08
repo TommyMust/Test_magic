976a2261d3e7f98ae330c7312eee0b477b7a6193
LucaChena <116801200+LucaChena@users.noreply.github.com>
2022-12-08 16:44:31 +0100

    Add files via upload

---
+++ RecFaciale.py
@@ -0,0 +1,37 @@
+################################
+###########LIBRAIRIES###########
+################################
+import cv2
+
+################################
+########DETECTION OBJETS########
+################################
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+classNames = []
+classFile = "coco.names"
+with open(classFile, "rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+#print(classNames) Voir tour ce qu'il y a dans coco.names
+configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "frozen_inference_graph.pb"
+net = cv2.dnn_DetectionModel(weightsPath,configPath) #Bounding : rectangle de délimitations
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+def RecObjet() :
+    success,img = cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=0.5)
+    print(classIds,bbox)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)                         # Crée le rectange autour de l'objet
+            cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),   # crée le texte avec le nom de l'objet
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), thickness=2)
+            cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),  # crée le chiffre de précision
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0),thickness=2)
+            cv2.imshow("Output",img)
+            cv2.waitKey(1)
